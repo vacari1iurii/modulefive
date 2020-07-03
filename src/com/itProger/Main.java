2a74298a3a5e60556bba8cacbbaddef0f373f7e7
Vacari <vacari1iurii@gmail.com>
2020-07-03 23:50:53 +0300

    Module Five

---
+++ src/com/itProger/Main.java
@@ -0,0 +1,16 @@
+package com.itProger;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // в терминале пишем: git init
+        // логин и имейл уже установлены после урока
+        // создаём .gitignore в корневой папке (ModuleFive). в нём прописываем:
+                                                            *.txt
+                                                            bin/
+                                                            admin/
+                                                            config/
+        // git add .
+        // git commit -m "Module Five"
+    }
+}
